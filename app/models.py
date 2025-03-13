5ea1c0a3bb2d1668bc3d0b9bda4be67e22ee3819
Mercure95 <goasduffpierre93@gmail.com>
2025-03-13 12:09:22 +0100

    Ajout fichier Flask pour la BDD

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
+from datetime import datetime
+from app.extensions import db
+
+# User model
+class User(db.Model):
+	__tablename__ = 'users'
+
+	id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+	username = db.Column(db.String(255), nullable=False, unique=True)
+	email = db.Column(db.String(255), nullable=False, unique=True)
+	password_hash = db.Column(db.String(255), nullable=False)
+	full_name = db.Column(db.String(255), nullable=True)
+	name = db.Column(db.String(255), nullable=True)
 
