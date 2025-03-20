5f61eeadfd96b4783d0747b754293a3460a1a575
Mercure95 <goasduffpierre93@gmail.com>
2025-03-20 14:15:02 +0100

    Ajout des workflows + modifications de config.py et __init__.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,28 @@
 import os
+
+
 class Config:
 	SECRET_KEY = os.getenv('SECRET_KEY', 'password')
 	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:bonjour@localhost:9001/test_db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'enormemotdepasse')
+
+class DevelopmentConfig(Config): 
+    FLASK_ENV = 'development'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URL', 'postgresql://user:password@db:5432/basededev')
+
+class TestingConfig(Config):
+    FLASK_ENV = 'testing'
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL', 'postgresql://user:password@db:5432/basedeprod')
+
+class ProductionConfig(Config):
+    FLASK_ENV = 'production'
+    SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DATABASE_URL', 'postgresql://user:password@db:5432/basedeprod')
+
+config = { #dictionnaire des confs
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig
+}
