d7c49cb43a1e2a096fc345d6b5163d8f8d403e4e
goat du site <72492241+aguacero7@users.noreply.github.com>
2025-03-20 16:35:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 setup(
     name="depsec_models",
     version="0.1",
-    packages=["app"],
+    packages=["depsec_db],
     package_dir={"app": "app"},
     install_requires=["flask_sqlalchemy"],
 )
