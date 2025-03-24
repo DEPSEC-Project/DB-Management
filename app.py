e74952cba6bd326634bf8f637c81bc24b4ac1c21
goat du site <72492241+aguacero7@users.noreply.github.com>
2025-03-24 08:26:22 +0100

    fix: application renommée vers depsec_db

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, jsonify, render_template
+<<<<<<< Updated upstream
 from app import create_app
+=======
+
+from depsec_db import create_app
+
+>>>>>>> Stashed changes
 app = create_app()
 
 
